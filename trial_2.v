8e4c57d6c0611190e0e85d14c42eed4650cabfcc
shoaibahmed1085 <71823028+shoaibahmed1085@users.noreply.github.com>
2023-11-29 11:08:00 +0530

    Update trial_2.v

--- trial_2.v
+++ trial_2.v
@@ -1,2 +1,3 @@
 //this is a trial to see if updates are reflected
 //first line was visible, seeing second line after editing from git editor
+//second line was visible as a commit, seeing if this third line edited through NPP is visible post copying
